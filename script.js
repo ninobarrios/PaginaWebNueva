e251e8f1d7b93b0404188b6638e8f77686af593a
NinoBarrios <ninobbarrios.bellido@gmail.com>
2025-01-05 17:06:19 -0500

    agregado breakpoints

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function toggleMenu() {
-    document.getElementById('navbarMenu').classList.toggle('active');
-  }
+
+
+    document.getElementById('google_translate_button').addEventListener('click', function() {
+        var translateElement = document.getElementById('google_translate_element');
+        
+        if (translateElement.style.display === 'none') {
+            translateElement.style.display = 'block';
+        } else {
+            translateElement.style.display = 'none';
+        }
+    });
+
+    function toggleMenu() {
+        const navbar = document.querySelector('.navbar');
+        navbar.classList.toggle('active');
+    }
+    
